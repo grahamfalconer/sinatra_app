1f37937692395a2d45ef4aceba94d5ae0f9e9ada
Graham <grahamalconer@icloud.com>
2020-03-31 15:38:20 +0100

    Created form with key values

--- app.rb
+++ app.rb
@@ -6,11 +6,16 @@ end
 get '/secret' do
   "Harambe died for our sins :( "
 end
-get '/weapons_for_sale' do
-  "Ak-47 - $400 \n P90 - $100 "
+get '/shop' do
+  "Premium shirt - £30"
 end
-get '/cat' do
-  "<div style='border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-   </div>"
+get '/random_cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named_cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
 end
