abb17d17f75f93d13dbdd1b389545ee9b6e7490d
Graham <grahamalconer@icloud.com>
2020-03-31 16:11:55 +0100

    new form, named_cat is post response

--- app.rb
+++ app.rb
@@ -14,8 +14,12 @@ get '/random_cat' do
   erb(:index)
 end
 
-get '/named_cat' do
+post '/named_cat' do
   p params
   @name = params[:name]
   erb(:index)
+end
+
+get '/new_form' do
+  erb(:form)
 end
